673c739129b05022b417164857bb5af5b7fd1e0c
Guilherme Sotério <guilhermehenrique.oliv3@gmail.com>
2023-07-30 23:17:41 -0300

    removes console.log

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -2,11 +2,9 @@ import useSWR from 'swr'
 import api from "./api";
 
 export function useFetch<Data = any, Error = any>(slug: string) {
-  console.log("slug", slug)
     const { data, error } = useSWR<Data, Error>(`https://api.instabuy.com.br/apiv3/item?subdomain=supermercado&slug=${slug}`, async url => {
       const response = await api.get(url);
       return response.data;
     })
-    console.log("dataddff", data)
     return { data, error }
   }
